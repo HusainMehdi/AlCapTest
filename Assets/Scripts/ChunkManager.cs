c5b9936668939b0c80a6f3b0564591df06468e5a
Husain Mehdi <husmehdi@gmail.com>
2019-10-16 13:14:53 +0100

    Started level chunk loading
    *figure out the best way to implement this

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> chunkList = new List<GameObject>();
+    [SerializeField] public List<GameObject> chunkOrder = new List<GameObject>();
+    int currentChunk = 0;
+
+    // 1: load North chunk, 0: load South Chunk
+    public void LoadChunk(int dir)
+    {
+        if(dir == 1)
+        {
+            chunkList[currentChunk+1].SetActive(true);
+            currentChunk++;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Deactivate all chunks after the first
+        for (int i = 1; i < chunkList.Count; i++)
+        {
+            chunkList[i].SetActive(false); ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
